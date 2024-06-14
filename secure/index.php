8265dc8e9f55c3335589456dc0b2a8f03100ed95
Rey Dylanza <reydylanza@yahoo.com>
2024-06-14 21:37:43 +0700

    Add files via upload

---
+++ secure/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0;url=shop/index.php">
+    <title>Redirect</title>
+</head>
+<body>
+    <p>Mengarahkan ke dashboard...</p>
+</body>
+</html>
